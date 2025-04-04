c83d84d7de837da14658b00951bed8a558ec0249
pcbrom <pcbrom@gmail.com>
2025-04-04 11:50:38 -0300

    update

--- res_claude.py
+++ res_claude.py
@@ -13,8 +13,8 @@ dotenv_path = "/mnt/4d4f90e5-f220-481e-8701-f0a546491c35/arquivos/projetos/.env"
 load_dotenv(dotenv_path=dotenv_path)
 
 # Access and store the environment variable
-xai_api_key = os.getenv("XAI_API_KEY")
-client = anthropic.Anthropic()
+claude_api_key = os.getenv("ANTHROPIC_API_KEY")
+client = anthropic.Anthropic(api_key=claude_api_key)
 model = 'claude-3-7-sonnet-20250219'
 
 
